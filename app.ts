0e83c2d3235728d026aaad1af279fe7912d017b4
Alireza Abdollahi <mra.abdollahi@gmail.com>
2022-07-03 14:36:20 +0430

    feat: add cors

--- app.ts
+++ app.ts
@@ -1,13 +1,21 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
+const port = process.env.PORT;
+const corsOptions = {
+  origin: `https://localhost:${port}`,
+};
 
 const app: Express = express();
-const port = process.env.PORT;
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.json({ message: "hello world" });
 });
 
 app.listen(port, () => {
